3fa1818c14ed1138e525bc697123384810faed52
Henrik Bråthen <henrik.brathen@bouvet.no>
2024-10-20 18:30:46 +0200

    move emacs init

---
+++ emacs/init.el
@@ -0,0 +1,34 @@
+(setq inhibit-startup-message t) ; Disable startup screen 
+(setq visible-bell t)            ; Visible bell
+
+(scroll-bar-mode -1) ; Disable visible scrollbar
+(tool-bar-mode -1)   ; Disable the toolbar
+(tooltip-mode -1)    ; Disable tooltips
+(set-fringe-mode 10) ; Give some breathing room
+(global-display-line-numbers-mode 1) ; Enable line numbers
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(Load-theme 'tango-dark)
+
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(which-key)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
