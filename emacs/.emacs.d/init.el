7c27148c8bd1246b3a63b40efb16e44b67898092
Henrik Bråthen <hsbrathen1@gmail.com>
2025-07-09 21:55:47 +0200

    feat: add osx PATH support and basic eglot setup for ts and js

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,7 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+(defconst *is-a-mac* (eq system-type 'darwin))
+
 ;; general settings
 (require 'init-fast-startup)
 (require 'init-packages)
@@ -9,6 +11,9 @@
 (require 'init-which-key)
 
 ;; init packages
+(when *is-a-mac*
+    (require 'init-osx))
+(require 'init-eglot)
 (require 'init-git)
 (require 'init-org)
 (require 'init-vertico)
